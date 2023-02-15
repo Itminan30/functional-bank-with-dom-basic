bf0bfbca3dd922b1c0f57b09f8eaefdba09635ed
Itminan30 <itminan30@gmail.com>
2023-02-15 19:23:22 +0600

    withdraw overflow handled

--- script/withdraw.js
+++ script/withdraw.js
@@ -2,8 +2,14 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
     const previousWithdrawTotal = getTextElementValueById('withdraw-total');
     const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    setTextElementValueById('withdraw-total', newWithdrawTotal);
     const previousBalanceTotal = getTextElementValueById('balance-total');
-    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-    setTextElementValueById('balance-total', newBalanceTotal);
+    if(previousBalanceTotal>=newWithdrawAmount){
+        setTextElementValueById('withdraw-total', newWithdrawTotal);
+        const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+        setTextElementValueById('balance-total', newBalanceTotal);
+    }
+    else{
+        alert("Don't Have Enough Balance");
+    }
+    
 })
